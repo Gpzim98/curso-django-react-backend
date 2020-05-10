ebfec28138a6691b611dec11a4dde9943b9583df
Gregory Pacheco <gpzim98@gmail.com>
2020-05-10 19:20:34 +0100

    Filtering the List queryset by the logged user

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ from core.views import ListViewSet, ItemViewSet
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
-router.register(r'list', ListViewSet)
+router.register(r'list', ListViewSet, basename='list')
 router.register(r'item', ItemViewSet)
 
 urlpatterns = [
--- core/views.py
+++ core/views.py
@@ -5,11 +5,13 @@ from .models import List, Item
 
 
 class ListViewSet(viewsets.ModelViewSet):
-    queryset = List.objects.all()
     serializer_class = ListSerializer
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
 
+    def get_queryset(self):
+        user = self.request.user
+        return List.objects.filter(owner=user)
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
