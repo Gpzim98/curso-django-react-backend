bc250335ca98ecec0c79355ef80f33470226a164
Gregory Pacheco <gpzim98@gmail.com>
2020-06-07 19:18:49 +0100

    Adding support to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -53,9 +53,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
-    'default': {
+    'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoreact',
+        'USER': 'djangoreact',
+        'PASSWORD': 'khjgvhkjlGKJFJHGK(*&^%$$%^',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
